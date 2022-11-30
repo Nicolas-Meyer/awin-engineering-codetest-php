db7da8eeb786b688be5d45c789480978419dac2a
Nicolas <nicolas@spitfireaudio.com>
2022-11-30 07:59:06 +0100

    add very basic tests for CoffeeBreakPreferenceController

---
+++ src/Awin/Tools/CoffeeBreak/Tests/Controllers/CoffeeBreakPreferenceControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Awin\Tools\CoffeeBreak\Tests\Controllers;
+
+use Awin\Tools\CoffeeBreak\Controller\CoffeeBreakPreferenceController;
+use Awin\Tools\CoffeeBreak\Entity\CoffeeBreakPreference;
+use Awin\Tools\CoffeeBreak\Entity\StaffMember;
+use Awin\Tools\CoffeeBreak\Repository\CoffeeBreakPreferenceRepository;
+use PHPUnit\Framework\TestCase;
+
+class CoffeeBreakPreferenceControllerTest extends TestCase
+{
+
+    public function testTodayActionReturnsJson()
+    {
+        $coffeeBreakPreferenceController = new CoffeeBreakPreferenceController();
+        $response = $coffeeBreakPreferenceController->todayAction('json', $this->getDefaultCoffeeBreakPreferenceRepository());
+        $this->assertEquals('200', $response->getStatusCode());
+        $this->assertJson($response->getContent());
+     }
+
+    public function testTodayActionReturnsXml()
+    {
+        $coffeeBreakPreferenceController = new CoffeeBreakPreferenceController();
+        $response = $coffeeBreakPreferenceController->todayAction('xml', $this->getDefaultCoffeeBreakPreferenceRepository());
+        $this->assertEquals('200', $response->getStatusCode());
+        $this->assertXmlStringEqualsXmlString('<expectedXml/>', $response->getContent());
+
+    }
+
+    public function testTodayActionReturnsHtml()
+    {
+        $coffeeBreakPreferenceController = new CoffeeBreakPreferenceController();
+        $response = $coffeeBreakPreferenceController->todayAction('html', $this->getDefaultCoffeeBreakPreferenceRepository());
+        $this->assertEquals('200', $response->getStatusCode());
+        $this->assertXmlStringEqualsXmlString('<expectedXml/>', $response->getContent());
+
+    }
+
+    public function testTodayActionDefaultsToHtml()
+    {
+        $coffeeBreakPreferenceController = new CoffeeBreakPreferenceController();
+        $response = $coffeeBreakPreferenceController->todayAction('', $this->getDefaultCoffeeBreakPreferenceRepository());
+        $this->assertEquals('200', $response->getStatusCode());
+        $this->assertXmlStringEqualsXmlString('<expectedXml/>', $response->getContent());
+
+    }
+
+    public function testTodayThrowsExceptionWhenUnknownFormat()
+    {
+
+        $coffeeBreakPreferenceController = new CoffeeBreakPreferenceController();
+        $this->expectException(\InvalidArgumentException::class);
+        $response = $coffeeBreakPreferenceController->todayAction('someFormat', $this->getDefaultCoffeeBreakPreferenceRepository());
+
+    }
+
+    private function getDefaultCoffeeBreakPreferenceRepository()
+    {
+
+        $staffMember = new StaffMember();
+        $staffMember->setSlackIdentifier("ABC123");
+
+        $coffeeBreakPreferenceRepository = $this->createMock(CoffeeBreakPreferenceRepository::class);
+        $coffeeBreakPreferenceRepository->expects(self::once())
+            ->method('getPreferencesForToday')
+            ->willReturn(new CoffeeBreakPreference('food','toast', $staffMember));
+
+        return $coffeeBreakPreferenceRepository;
+
+    }
+
+}
